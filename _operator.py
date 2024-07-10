40091f4a654aba78b8075b9f083431612ebdd764
“ijassm” <ijass.ocean@gmail.com>
2024-07-10 19:02:22 +0530

    Completed datatype, variable and started operator

---
+++ _operator.py
@@ -0,0 +1,33 @@
+# Arithmetic operator
+
+# print(5 + 5)
+# print(5 - 5)
+# print(5 / 2)
+# print(5 // 2)
+# print(5 % 2)
+# print(5**2)
+# print(5 * 2)
+
+# Assignment operator
+
+# a = 5
+
+# # a = a + 1
+# a += 1
+
+# print(a)
+
+# Comparison operator
+
+# print(2 > 4)
+# print(2 < 4)
+# print(0 >= -99999999999999999)
+# print(-1 >= -99999999999999999)
+# print(1 >= 1)
+# print(1 <= 1)
+# print(1 != 1)
+# print(1 == 1)
+# print(1 == "1")
+
+
+# print("234243")
---
+++ datatype&variables.py
@@ -0,0 +1,68 @@
+# Basic datatypes
+
+'''
+print(type(12))
+print(type(12.55))
+print(type(12.5j))
+print(type("hello python"))
+print("hello python")
+print(
+    """hello 
+      
+      python"""
+)
+print(
+    """hello 
+      
+      python"""
+)
+print(type(True))
+print(type(False))
+'''
+
+# Variables
+# case sensitive
+# a = "hello"
+# A = "hi"
+# print(a)
+# print(type(a))
+# print(A)
+# print(type(A))
+
+# a = 10
+# print(a)
+# print(type(a))
+
+
+# naming convention
+# name = "xyz"
+# age = 10
+# location = "pondy"
+# voteEligibility = False
+
+
+# print("name =", name)
+
+# print("age =", age)
+
+# print("location =", location)
+
+# print("vote eligibility =", voteEligibility)
+
+# swap the variable ?
+
+# a = 1
+# b = 2
+
+# b = b - a
+# a = a + b
+
+# temp = a
+# a = b
+# b = temp
+
+# a, b = b, a
+
+# print("a =", a)
+# print("b =", b)
+
