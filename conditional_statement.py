0d72d085974069980860debc09e9f984ce05570b
“ijassm” <ijass.ocean@gmail.com>
2024-07-15 19:08:51 +0530

    Completed if conditions with elif

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,13 +1,41 @@
-sysUsername = "mathew"
-sysPassword = "12345"
+# sysUsername = "mathew"
+# sysPassword = "12345"
 
-username = input("Enter your username : ")
-password = input("Enter your password: ")
+# username = input("Enter your username : ")
+# password = input("Enter your password: ")
 
-print(username)
-print(password)
+# print(username)
+# print(password)
 
-if username == sysUsername and password == sysPassword:
-    print("Your logged in successfully😊")
+# if username == sysUsername and password == sysPassword:
+#     print("Your logged in successfully😊")
+# else:
+#     print("Invalid credentials! ��")
+
+
+# amount = 1000
+
+# note2000 = amount // 2000
+# amount = amount - (2000 * note2000)
+# note500 = amount // 500
+# amount = amount - (500 * note500)
+# note200 = amount // 200
+# amount = amount - (200 * note200)
+
+# print("RS 2000 - ", note2000)
+# print("RS 500  - ", note500)
+# print("RS 200  - ", note200)
+# print("remaining amount  - ", amount)
+
+a = 20
+b = 20
+c = 20
+
+if a == b == c:
+    print("a b c are equal")
+elif a > b and a > c:
+    print("a is greater than b and c")
+elif b > c:
+    print("b is greater than c and a")
 else:
-    print("Invalid credentials! ��")
+    print("c is greater than a and b")
