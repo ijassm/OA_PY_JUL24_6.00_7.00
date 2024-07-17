524d37e8e7592ed31628b2a5be7c15858f69971f
“ijassm” <ijass.ocean@gmail.com>
2024-07-17 19:06:44 +0530

    Completed nested conditions

--- conditional_statement.py
+++ conditional_statement.py
@@ -27,15 +27,70 @@
 # print("RS 200  - ", note200)
 # print("remaining amount  - ", amount)
 
-a = 20
-b = 20
+# a = 20
+# b = 20
+# c = 20
+
+# if a == b == c:
+#     print("a b c are equal")
+# elif a > b and a > c:
+#     print("a is greater than b and c")
+# elif b > c:
+#     print("b is greater than c and a")
+# else:
+#     print("c is greater than a and b")
+
+
+# units = 251
+# amount = 0
+
+# print("Units used - ", units)
+
+# amount = amount + (50 * 0.50)
+# units -= 50
+# print(f"units till 50", amount)
+
+# amount = amount + (100 * 0.75)
+# units -= 100
+# print(f"for next 100 units", amount)
+
+# amount = amount + (100 * 1.20)
+# units -= 100
+# print(f"for next 100 units", amount)
+
+# amount = amount + (units * 1.50)
+# units = 0
+# print(f"For unit above 250", amount)
+
+# print(f"remaining units", units)
+# print(f"total amount -", amount)
+
+
+# print("An additional surcharge of 20% is added to the bill : ", amount * 0.20)
+
+# Nested if
+
+a = 30
+b = 22
 c = 20
 
 if a == b == c:
     print("a b c are equal")
 elif a > b and a > c:
-    print("a is greater than b and c")
+    print("a is greater than all")
+    if b < c:
+        print("b is lesser than all")
+    else:
+        print("c is lesser than all")
 elif b > c:
-    print("b is greater than c and a")
+    print("b is greater than all")
+    if a < c:
+        print("a is lesser than all")
+    else:
+        print("c is lesser than all")
 else:
-    print("c is greater than a and b")
+    print("c is greater than all")
+    if b < a:
+        print("b is lesser than all")
+    else:
+        print("a is lesser than all")
