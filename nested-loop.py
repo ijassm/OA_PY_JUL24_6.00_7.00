c6a8b3436ea425af7b2358baf1b33f0f2d2d19c1
“ijassm” <ijass.ocean@gmail.com>
2024-09-18 19:14:35 +0530

    Completed nested for loop

---
+++ nested-loop.py
@@ -0,0 +1,53 @@
+# for i in range(1, 3):
+#     for j in range(1, 3):
+#         for k in range(1, 3):
+#             print(i, j, k)
+
+
+# i = 1
+#     j = 1 | j = 2
+# i = 2
+#     j = 1 | j = 2
+
+# ------------------
+
+# i = 1
+#     j = 1
+#         k = 1 | k = 2
+#     j = 2
+#         k = 1 | k = 2
+
+# i = 2
+#     j = 1
+#         k = 1 | k = 2
+#     j = 2
+#         k = 1 | k = 2
+
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print("#", end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(i, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(j, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         if j % 2 == 0:
+#             print(0, end=" ")
+#         else:
+#             print(1, end=" ")
+#     print()
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print()
