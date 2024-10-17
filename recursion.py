8da3f59d622c99d9f17ff6647da926a2b970866d
ijassm <ijass.ocean@gmail.com>
2024-10-17 19:19:35 +0530

    stack and recursion completed

---
+++ recursion.py
@@ -0,0 +1,16 @@
+# def recursion(i):
+#     if i == 0:
+#         return
+#     else:
+#         recursion(i - 1)
+#         print(i)
+
+
+# recursion(10)
+
+
+# sum(2)(5)(3)(6)..........()
+# sum(2)(5)(3)(6)() => 16
+# sum(2)(5)(3)() => 10
+# sum(2)(5)() => 7
+# sum(2)() => 2
---
+++ stack.py
@@ -0,0 +1,35 @@
+# def first():
+#     print("First")
+#     return 1
+
+
+# def second():
+#     a = first()
+#     print("Second")
+#     return a
+
+
+# def third():
+#     a = second()
+#     print("Third")
+#     return a
+
+
+# print(third())
+
+
+# def first():
+#     print("First")
+
+
+# def second():
+#     print("Second")
+
+
+# def third():
+#     print("Third")
+
+
+# first()
+# second()
+# third()
