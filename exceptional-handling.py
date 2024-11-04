4f8e7d4216a98b580f25a052a551a98caedcd64d
ijassm <ijass.ocean@gmail.com>
2024-11-04 19:29:58 +0530

    completed exceptional handling

---
+++ exceptional-handling.py
@@ -0,0 +1,39 @@
+# Type Error
+# x = 5
+# y = "hello"
+# z = x + y
+
+# Zero Division Error
+# x = 5
+# y = 0
+# z = x / y
+
+# Name Error
+# print(b)
+
+
+# try:
+#     # Zero Division Error
+#     x = int(input("Enter a num1: "))
+#     y = int(input("Enter a num1: "))
+#     z = x / y
+#     print(z + 5)
+# except ZeroDivisionError:
+#     print("You can't divide by zero✌")
+# except NameError:
+#     print("please check your variable is defined🤞")
+# except Exception as e:
+#     print("Something went wrong🤞", e)
+# else:
+#     print("Everything went well")
+# finally:
+#     print("This code will always be executed")
+
+
+# age = int(input("Enter your age: "))
+
+# if age < 0:
+#     raise Exception("Please enter a valid age")
+
+
+# print("Next line code is executed")
